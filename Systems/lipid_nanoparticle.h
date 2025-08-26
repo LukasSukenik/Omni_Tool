f81ac3afa76b627f6e8b90587d635269f3cdd7ce
LukasSukenik <sukeniklukas@gmail.com>
2025-08-26 13:22:28 +0200

    Lipid Nano Water Analysis

--- Systems/lipid_nanoparticle.h
+++ Systems/lipid_nanoparticle.h
@@ -11,6 +11,8 @@ public:
     inline static const string keyword = "Lipid_Nanoparticle";
     const string name = "Lipid_Nanoparticle";
 
+    Force_Field ff;
+
     Lipid_Nanoparticle() : System_Base("Lipid_Nanoparticle") {}
 
     void execute(Data& data)
@@ -22,6 +24,12 @@ public:
             // feeler atom
             Atom feeler;
             feeler.pos = Tensor_xyz(0.0, 0.0, 0.0);
+            feeler.type = 2; // lipid tail
+
+            // set ff
+            ff.lj[1] = LJ(-0.8);
+            ff.lj[2] = LJ(2.8);
+            ff.lj[3] = LJ(2.8);
 
             // box boundaries
             cerr << data.in.sim_box.xlo << " " << data.in.sim_box.xhi << endl;
@@ -29,24 +37,42 @@ public:
             // trajectory load
             Trajectory traj;
             traj.load(data.in.trajectory);
-            Atoms last_frame;
+
+            int sys_id = data.id_map[data.in.id];
+            Atoms& last_frame = data.coll_beads[sys_id];
             last_frame.set_frame(traj[ traj.size()-1 ]);
+            cerr << "Trajectory loaded" << endl;
 
-            // feel stuff, similar to monte carlo pi calculation
+            double step_size = 1.0;
+            int water_count = 0;
+            int membrane_count = 0;
 
-            // example for a single location
-            if( is_water(last_frame, feeler) )
-            {
-                cerr << "location is water" << endl;
+            double range = data.in.sim_box.xhi - data.in.sim_box.xlo;
+            for(double x=data.in.sim_box.xlo; x<data.in.sim_box.xhi; x+= step_size) {
+                cerr << "\r" << 100.0 * (x - data.in.sim_box.xlo) / range << " %         " << flush;
+                for (double y=data.in.sim_box.ylo; y<data.in.sim_box.yhi; y+=step_size) {
+                    for (double z=data.in.sim_box.zlo; z<data.in.sim_box.zhi; z+= step_size) {
+                        feeler.pos = Tensor_xyz(x, y, z);
+                        if( is_water(last_frame, feeler) )
+                        {
+                            water_count++;
+                        }
+                        else
+                        {
+                            membrane_count++;
+                        }
+                    }
+                }
             }
-            else
-            {
-                cerr << "location is membrane" << endl;
-            }
 
-            // distribute location through box
-            // count water and membrane location
-            // profit?
+            if (membrane_count != 0) {
+                double ratio = static_cast<double>(water_count) / membrane_count;
+                cout << "Water to Membrane ratio:" << ratio << endl;
+                cout << "Membrane %:" << 100.0 * membrane_count / (membrane_count + water_count) << endl;
+                cout << "step size:" << step_size << endl;
+            } else {
+                cerr << "Membrane count is zero, cannot compute ratio." << endl;
+            }
 
             cerr << "calc_water_content::end" << endl;
         }
